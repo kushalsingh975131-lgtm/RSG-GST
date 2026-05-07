220d3dc6ff6bc27384187fe26dc7af7bcff7e6bf
kushalsingh975131-lgtm <kushalsingh975131@gmail.com>
2026-05-07 13:59:48 +0000

    fetch 4

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -87,9 +87,9 @@ const fetchGSTINDetails = async (gstin: string) => {
         headers: { 'X-API-Key': import.meta.env.VITE_GSTIN_API_KEY },
       }
     );
-    console.log('Status:', res.status);
-    const data1 = await res.json();
-    console.log('Data:', data1);
+    //console.log('Status:', res.status);
+   // const data1 = await res.json();
+    //console.log('Data:', data1);
     if (!res.ok) {
       setGstinError('Unable to fetch details');
       return;
